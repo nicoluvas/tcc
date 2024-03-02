bdefb0e7ae328037e6ec67fd90b5df33c878ab9f
nicoluvas <nicolaspoficial@gmail.com>
2024-03-02 20:17:24 -0300

    correção de bugs e a tela de login ta fluindo agora slk

--- app/indice/indice.js
+++ app/indice/indice.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('.theme');
+const banner = document.querySelector('#bg-banner');
+const bannerDark = document.querySelector('#bg-banner-dark');
 
 button.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-})
+  document.body.classList.toggle('dark-mode');
+  banner.classList.toggle('hidden');
+  bannerDark.classList.toggle('hidden');
+});
