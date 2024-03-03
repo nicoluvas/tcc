0aab653fb8fd4fd976fcdefb47876738cb13cd44
nicoluvas <nicolaspoficial@gmail.com>
2024-03-03 13:24:35 -0300

    ai

--- app/indice/indice.js
+++ app/indice/indice.js
@@ -1,9 +1,21 @@
-const button = document.querySelector('.theme');
-const banner = document.querySelector('#bg-banner');
-const bannerDark = document.querySelector('#bg-banner-dark');
+const button = document.querySelector(".theme");
+const banner = document.querySelector("#bg-banner");
+const bannerDark = document.querySelector("#bg-banner-dark");
+const logo = document.querySelector("#logo");
+const logoDark = document.querySelector("#logo-dark");
 
-button.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  banner.classList.toggle('hidden');
-  bannerDark.classList.toggle('hidden');
+button.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  toggleMode();
 });
+function toggleMode() {
+  banner.classList.toggle("hidden");
+  banner.classList.toggle("shown");
+  bannerDark.classList.toggle("hidden");
+  bannerDark.classList.toggle("shown");
+
+  logo.classList.toggle("hidden");
+  logo.classList.toggle("shown");
+  logoDark.classList.toggle("hidden");
+  logoDark.classList.toggle("shown");
+}
