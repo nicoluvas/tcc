cc6531566df9c10eade86ebcd497093f82b3695b
lemelinha <lucas.leme.0806@gmail.com>
2024-04-17 09:07:59 -0300

    header da home refeita (nao feita)

--- App/Views/Index/index.php
+++ App/Views/Index/index.php
@@ -10,51 +10,37 @@
 </head>
 
 <body id="home">
-  <div class="pagina1">
   <header>
     <nav>
-      <img id="logo" src="/assets/images/index/logo.png" class="shown logo">
-      <img id="logo-dark" src="/assets/images/index/logo-dark.png" class="hidden logo">
-      <ul>
-        <li>
-          <a href="#">Quem Somos</a>
-        </li>
-        <li>
-          <a href="#">Fale Conosco</a>
-        </li>
-        <li>
-          <a href="#">Método de Ensino</a>
-        </li>
-      </ul>
-      <div class="point theme">
-        <svg class="svg-lua-bola" width="46" height="46" viewBox="0 0 46 46" fill="none">
-          <circle cx="23" cy="23" r="23" />
-        </svg>
-        <svg class="svg-lua" width="24" height="25" viewBox="0 0 24 25" fill="none">
-          <path fill-rule="evenodd" clip-rule="evenodd"
-            d="M11.5258 0.0251729C8.27247 0.283094 4.87561 2.09221 2.8349 4.65375C1.52552 6.29743 0.674884 8.08753 0.251457 10.0907C0.0726542 10.9365 0 11.66 0 12.5949C0 13.7983 0.165361 14.8375 0.562681 16.1305C2.02776 20.8989 6.30314 24.4238 11.2758 24.9631C11.9699 25.0385 13.7317 24.9923 14.4815 24.8792C17.2049 24.4684 19.6866 23.1644 21.83 21.0179C22.4812 20.3658 23.0568 19.6523 23.6276 18.7897L24 18.2268L23.8073 18.0421C23.7012 17.9404 23.6023 17.8573 23.5874 17.8573C23.5726 17.8573 23.3647 17.9936 23.1255 18.1603C21.9103 19.0068 20.453 19.5928 19.0452 19.8012C17.0221 20.1007 14.7965 19.5309 13.0476 18.2657C11.3305 17.0235 10.1906 15.2836 9.74894 13.2304C9.62291 12.6446 9.57847 11.2823 9.66307 10.5992C9.85587 9.04326 10.6476 7.33887 11.7303 6.14903C13.9401 3.72042 16.8546 2.82551 20.1632 3.55961C20.5145 3.63753 20.8442 3.71371 20.8958 3.72883C21.0317 3.76872 21.136 3.54911 21.0608 3.38142C20.9814 3.20435 20.0916 2.48679 19.3977 2.04018C17.8504 1.04444 15.95 0.335682 14.2434 0.117936C13.3893 0.00897888 12.2296 -0.0306319 11.5258 0.0251729Z" />
-        </svg>
-        </btn>
+      <div class="logo">
+        <img class="logo-png">
       </div>
-      <btn class="btn-restrito point">
-        <a href="/login">Área Restrita</a>
-      </btn>
+      <ul>
+        <li>Contato</li>
+        <li>Metodo</li>
+        <li>???</li>
+      </ul>
+      <button class="tema"></button>
+      <button class="restrito">Área Restrita</button>
     </nav>
   </header>
-  
-  <div>
-    <img id="bg-banner" class="shown bg-banner" src="/assets/images/index/mesh-gradient.png">
-    <img id="bg-banner-dark" class="hidden bg-banner" src="/assets/images/index/mesh-gradient-dark.png">
-  </div>
-  <img class="shadow wein" src="/assets/images/index/wein.png">
-  <img class="lampada" src="/assets/images/index/lampada.png">
+  <div class="pagina1">
 
-  <div class="resolva">
-    <h1>Resolva o seu <br> <span class="marked">futuro</span> com <br> <span class="highlight">Bhaskara</span>!</h1>
-    <h3>Conheça a melhor <span class="weight">escola</span> do país</h3>
-  </div>
 
-  <!-- <div class="boxes">
+
+    <div>
+      <img id="bg-banner" class="shown bg-banner" src="/assets/images/index/mesh-gradient.png">
+      <img id="bg-banner-dark" class="hidden bg-banner" src="/assets/images/index/mesh-gradient-dark.png">
+    </div>
+    <img class="shadow wein" src="/assets/images/index/wein.png">
+    <img class="lampada" src="/assets/images/index/lampada.png">
+
+    <div class="resolva">
+      <h1>Resolva o seu <br> <span class="marked">futuro</span> com <br> <span class="highlight">Bhaskara</span>!</h1>
+      <h3>Conheça a melhor <span class="weight">escola</span> do país</h3>
+    </div>
+
+    <!-- <div class="boxes">
 
     <div class="box0">
       <p class="desc">meu metodo</p>
