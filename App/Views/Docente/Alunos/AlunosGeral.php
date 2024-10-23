21c97377a41365fc4ac775d6342b85213eb4e940
nicoluvas <nicolaspoficial@gmail.com>
2024-10-22 22:08:25 -0300

    p -> select

--- App/Views/Docente/Alunos/AlunosGeral.php
+++ App/Views/Docente/Alunos/AlunosGeral.php
@@ -1,48 +1,47 @@
 <h2>Alunos</h2>
 <style>
-      @import url('/assets/css/global.css');
-    main  div > a {
-    padding: 2px 5px 2px 5px;
-      border: 1px solid var(--shadow);
-      border-radius: 10px;
-      background-color: var(--shadow);
-    }
-    main .alunos {
-        display: flex;
-        flex-direction: column;
-        row-gap: 8px;
-    }
-    main .aluno+.aluno {
-        border-top: 1px solid #c2c2c2;
-    }
+@import url('/assets/css/global.css');
+
+main .alunos {
+    display: flex;
+    flex-direction: column;
+    row-gap: 8px;
+}
+
+.turmas {
+    margin: 1rem;
+}
 </style>
-<?php
-    foreach($this->turmas as $turma) :
-        ?>
-            <a href="/docente/alunos/turma/<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></a>
-        <?php
-    endforeach;
-?>
+<div class="turmas">
+    <select id="turmasSelect" onchange="location = this.value;">
+        <option value="">Selecione a turma</option>
+        <?php foreach($this->turmas as $turma): ?>
+        <option value="/docente/alunos/turma/<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></option>
+        <?php endforeach; ?>
+    </select>
+</div>
+
 
 <div class="alunos">
     <?php
     foreach($this->alunos as $aluno):
         ?>
-            <div class="aluno" id="<?= $aluno->cd_aluno ?>">
-                <p><?= $aluno->nome_aluno ?></p>
-                <p>Turma <?= $aluno->nm_turma ?></p>
-                <a href="/docente/aluno/<?= $aluno->cd_aluno ?>/info">Informações</a>
-                <button class="cancelarMatricula" aluno="<?= $aluno->nome_aluno ?>" id="<?= $aluno->cd_aluno ?>">Cancelar Matrícula</button>
-            </div> 
-            <?php
+    <div class="aluno" id="<?= $aluno->cd_aluno ?>">
+        <p><?= $aluno->nome_aluno ?></p>
+        <p>Turma <?= $aluno->nm_turma ?></p>
+        <a href="/docente/aluno/<?= $aluno->cd_aluno ?>/info">Informações</a>
+        <button class="cancelarMatricula" aluno="<?= $aluno->nome_aluno ?>" id="<?= $aluno->cd_aluno ?>">Cancelar
+            Matrícula</button>
+    </div>
+    <?php
     endforeach;
     ?>
 </div>
 
 <script>
-    $('.aluno button.cancelarMatricula').click(function() {
-        if (confirm('Deseja cancelar a matrícula de ' + $(this).attr('aluno') + '?')) {
-            $.ajax({
+$('.aluno button.cancelarMatricula').click(function() {
+    if (confirm('Deseja cancelar a matrícula de ' + $(this).attr('aluno') + '?')) {
+        $.ajax({
                 url: `/docente/aluno/${$(this).attr('id')}/cancelar-matricula`,
                 type: 'POST',
                 dataType: 'json'
@@ -55,8 +54,7 @@
                 }
                 alert('Algo deu errado!')
             })
-        }
-        return false; // cancel the event
-    });
+    }
+    return false; // cancel the event
+});
 </script>
-
--- App/Views/Docente/Docente/ListarDocente.php
+++ App/Views/Docente/Docente/ListarDocente.php
@@ -1,20 +1,25 @@
 <h2>Docentes</h2>
 <style>
-      @import url('/assets/css/global.css');
-    main  div > a {
+@import url('/assets/css/global.css');
+
+main div>a {
     padding: 2px 5px 2px 5px;
-      border: 1px solid var(--shadow);
-      border-radius: 10px;
-      background-color: var(--shadow);
-    }
-    main .docentes {
-        display: flex;
-        flex-direction: column;
-        row-gap: 8px;
-    }
-    main .docente+.docente {
-        border-top: 1px solid #c2c2c2;
-    }
+    border: 1px solid var(--shadow);
+    border-radius: 10px;
+    background-color: var(--shadow);
+}
+
+main .docentes {
+    display: flex;
+    flex-direction: column;
+    row-gap: 8px;
+}
+
+.docente {
+    border: 1px solid var(--shadow);
+    border-radius: 1rem;
+    padding: 1rem;
+}
 </style>
 
 <div class="docentes">
@@ -22,21 +27,22 @@
         $cargos = ['2' => 'Professor', '3' => 'Secretário', '4' => 'Coordenador', '5' => 'Diretor'];
         foreach ($this->docentes as $docente):
             ?>
-                <div class="docente" id="<?= $docente->cd_docente ?>">
-                    <p><?= $docente->nome_docente ?></p>
-                    <p>Cargo: <?= $cargos[$docente->id_cargo] ?></p>
-                    <a href="/docente/docente/<?= $docente->cd_docente ?>/info">Informações</a>
-                    <button class="desligarDocente" docente="<?= $docente->nome_docente ?>" id="<?= $docente->cd_docente ?>" <?= $docente->id_cargo>=$_SESSION['logged']['cargo']?'disabled':'' ?>>Desligar Docente</button>
-                </div>
-            <?php
+    <div class="docente" id="<?= $docente->cd_docente ?>">
+        <p><?= $docente->nome_docente ?></p>
+        <p>Cargo: <?= $cargos[$docente->id_cargo] ?></p>
+        <a href="/docente/docente/<?= $docente->cd_docente ?>/info">Informações</a>
+        <button class="desligarDocente" docente="<?= $docente->nome_docente ?>" id="<?= $docente->cd_docente ?>"
+            <?= $docente->id_cargo>=$_SESSION['logged']['cargo']?'disabled':'' ?>>Desligar Docente</button>
+    </div>
+    <?php
         endforeach;
     ?>
 </div>
 
 <script>
-    $('.docente button.desligarDocente').click(function() {
-        if (confirm('Deseja desligar o docente ' + $(this).attr('docente') + '?')) {
-            $.ajax({
+$('.docente button.desligarDocente').click(function() {
+    if (confirm('Deseja desligar o docente ' + $(this).attr('docente') + '?')) {
+        $.ajax({
                 url: `/docente/docente/${$(this).attr('id')}/desligar`,
                 type: 'POST',
                 dataType: 'json'
@@ -55,7 +61,7 @@
             .catch(a => {
                 console.log(a)
             })
-        }
-        return false; // cancel the event
-    });
+    }
+    return false; 
+});
 </script>
