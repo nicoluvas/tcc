0966bfbab4da808cd81e32fc898fc3674bf4756d
nicoluvas <nicolaspoficial@gmail.com>
2024-10-24 16:46:34 -0300

    +periodo letivo ativo +chamada e notas finalizado joia

--- App/Views/Professor/chamada.php
+++ App/Views/Professor/chamada.php
@@ -1,67 +1,79 @@
 <style>
-    @import url('global.css');
-    textarea {
-        background-color: var(--background);
-        border: 1px var(--shadow) solid;
-        border-radius: .5rem;
-        min-width: 30vw;
-        min-height: 30vh;
-        padding: 1vh;
-    }
+@import url('global.css');
 
-    .chmda{
-        width: 75vw;
-        padding: 1vw;
-        background-color: var(--background-alt)
-    }
+textarea {
+    background-color: var(--background);
+    border: 1px var(--shadow) solid;
+    border-radius: .5rem;
+    min-width: 30vw;
+    min-height: 30vh;
+    padding: 1vh;
+}
 
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+select {
+    padding: 1rem;
+    background-color: var(--accent);
+    color: var(--text);
+    font-weight: 600;
+    border-radius: .5rem;
+    border: none;
+}
 
-    input {
-        padding: 1rem;
+.chmda {
+    width: 75vw;
+    padding: 1vw;
+    background-color: var(--background-alt)
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+input {
+    padding: 1rem;
     background-color: var(--accent);
     border-radius: 1rem;
     padding: 1rem;
     margin: 0.5rem;
     border: none;
     font-weight: 600;
+
     &:hover {
         opacity: 0.5;
         cursor: pointer;
     }
-    }
+}
 
-    .selects{
-            display: flex;
-            justify-content: center;
-            padding: 1rem;
-            column-gap: 1rem;
-        }
-        .selects>select{
-            padding: 1rem;
-        }
+.selects {
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    column-gap: 1rem;
+}
 
-    .chmda{
-        display: flex;
-        justify-content: space-evenly;
-        border: 1px solid var(--shadow);
-        border-radius: .5rem;
-    }
+.selects>select {
+    padding: 1rem;
+}
 
-    .chmda-dir{
-        display:flex;
-        flex-direction:column;
-    }
+.chmda {
+    display: flex;
+    justify-content: space-evenly;
+    border: 1px solid var(--shadow);
+    border-radius: .5rem;
+}
+
+.chmda-dir {
+    display: flex;
+    flex-direction: column;
+}
 </style>
 <?php
     if (!EM_PERIODO_LETIVO) {
         ?>
-            <p>fora de periodo letivo</p>
-        <?php
+<p style="display:flex; justify-content:center; font-size: 10vh;">Fora de período letivo. 😐</p>
+<?php
         exit();
     }
 ?>
@@ -73,8 +85,8 @@
                 foreach($this->turmas as $turma):
                     if (!array_key_exists($turma->cd_turma, $this->professor_materias_turmas)) continue;
                     ?>
-                        <option value="<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></option>
-                        <?php
+            <option value="<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></option>
+            <?php
                 endforeach;
                 ?>
         </select>
@@ -91,8 +103,8 @@
                 endforeach;
                 foreach($aux as $id_materia => $materia):
                     ?>
-                        <option value="<?= $id_materia ?>"><?= $materia ?></option>
-                    <?php
+            <option value="<?= $id_materia ?>"><?= $materia ?></option>
+            <?php
                 endforeach;
             ?>
         </select>
@@ -113,62 +125,62 @@
         </div>
     </div>
     <script>
-        let professor_materia = <?= json_encode($this->professor_materias_turmas) ?>;
+    let professor_materia = <?= json_encode($this->professor_materias_turmas) ?>;
 
-        $('form').on('submit', function (e) {
-            e.preventDefault()
+    $('form').on('submit', function(e) {
+        e.preventDefault()
 
-            $.ajax({
+        $.ajax({
                 'url': '/professor/chamada',
                 'type': 'POST',
                 'dataType': 'json',
                 'data': $(this).serialize()
             })
-            .done(function (data) {
+            .done(function(data) {
                 $('p.retorno').text(data.msg)
                 $('form').trigger("reset")
                 $('input[type="submit"]').prop('disabled', true)
             })
-            .catch(function (a) {
+            .catch(function(a) {
                 console.log(a)
             })
+    })
+
+    $('form select#turma').on('change', function() {
+        $('form select#turma option#init').remove()
+        $('form select#qt_aulas').prop('disabled', false)
+        $('form select#materia').prop('disabled', false)
+        $('form textarea').prop('disabled', false)
+        $('input[type="submit"]').prop('disabled', false)
+
+        let turma = $(this).val()
+        $('form select#materia option').each(function() {
+            if ($(this).val() in professor_materia[turma]) {
+                $(this).css('display', 'block')
+            } else {
+                $(this).css('display', 'none')
+            }
         })
-
-        $('form select#turma').on('change', function () {
-            $('form select#turma option#init').remove()
-            $('form select#qt_aulas').prop('disabled', false)
-            $('form select#materia').prop('disabled', false)
-            $('form textarea').prop('disabled', false)
-            $('input[type="submit"]').prop('disabled', false)
-
-            let turma = $(this).val()
-            $('form select#materia option').each(function () {
-                if ($(this).val() in professor_materia[turma]) {
-                    $(this).css('display', 'block')
-                } else {
-                    $(this).css('display', 'none')
-                }
-            })
-            $('form select#materia').val('')
-            $.ajax({
+        $('form select#materia').val('')
+        $.ajax({
                 'url': `/professor/chamada/alunos/turma/${$('form select#turma').val()}/qa/${$('form select#qt_aulas').val()}`,
                 'type': 'GET',
                 'dataType': 'html'
             })
-            .done(function (data) {
+            .done(function(data) {
                 $('.alunos').html(data)
             })
-        })
+    })
 
-        $('form select#qt_aulas').on('change', function () {
-            $.ajax({
+    $('form select#qt_aulas').on('change', function() {
+        $.ajax({
                 'url': `/professor/chamada/alunos/turma/${$('form select#turma').val()}/qa/${$('form select#qt_aulas').val()}`,
                 'type': 'GET',
                 'dataType': 'html'
             })
-            .done(function (data) {
+            .done(function(data) {
                 $('.alunos').html(data)
             })
-        })
+    })
     </script>
 </form>
--- App/Views/Professor/notas.php
+++ App/Views/Professor/notas.php
@@ -1,81 +1,153 @@
 <?php
     if (!EM_PERIODO_LETIVO) {
         ?>
-            <p>Fora de periodo letivo</p>
-        <?php
+<p style="display:flex; justify-content:center; font-size: 10vh;">Fora de período letivo. 😐</p>
+<?php
         die();
     }
 ?>
 <style>
-    select[readonly='readonly'] {
-        background: #eee; /*Simular campo inativo - Sugestão @GabrielRodrigues*/
-        pointer-events: none;
-        touch-action: none;
+@import url('/assets/css/global.css');
+main {
+    display:flex;
+    justify-content:center;
+}
+form {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background-color: var(--background);
+    min-width: 50vw;
+}
+
+input {
+    padding: 1vw;
+    background-color: var(--background);
+    min-height: 1rem;
+    border: none;
+    border-radius: .5rem;
+}
+
+select[readonly='readonly'] {
+    background: #eee;
+    pointer-events: none;
+    touch-action: none;
+}
+
+select {
+    padding: 1rem;
+    background-color: var(--accent);
+    color: var(--text);
+    font-weight: 600;
+    border-radius: .5rem;
+    border: none;
+}
+
+.selects {
+    margin: 1vw;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+button {
+    padding: 1rem;
+    font-weight: 500;
+    background-color: var(--accent);
+    border-radius: .5rem;
+    border:none;
+    max-height: fit-content;
+    max-width: fit-content; 
+    &:hover{
+        cursor:pointer;
+        opacity:.9;
     }
+}
 </style>
 <form>
-    <select name="turma" id="turma" required>
-        <option value="" id="init">Selecione uma turma</option>
-        <?php
-            foreach($this->turmas as $turma):
-                if (!array_key_exists($turma->cd_turma, $this->professor_materias_turmas)) continue;
-                ?>
-                    <option value="<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></option>
-                    <?php
-            endforeach;
-            ?>
-    </select>
-    <select name="materia" id="materia" required disabled>
-        <option value="">Selecione uma matéria</option>
-        <?php
-            $aux = [];
-            foreach($this->professor_materias_turmas as $turma):
-                foreach ($turma as $id_materia => $materia):
-                    if (array_key_exists($id_materia, $aux)) continue;
-                    $aux[$id_materia] = $materia;
-                    ksort($aux, SORT_NUMERIC);
+    <div class="selects">
+        <select name="turma" id="turma" required>
+            <option value="" id="init">Selecione uma turma</option>
+            <?php
+                foreach($this->turmas as $turma):
+                    if (!array_key_exists($turma->cd_turma, $this->professor_materias_turmas)) continue;
+                    ?>
+            <option value="<?= $turma->cd_turma ?>"><?= $turma->nm_turma ?></option>
+            <?php
                 endforeach;
-            endforeach;
-            foreach($aux as $id_materia => $materia):
                 ?>
-                    <option value="<?= $id_materia ?>"><?= $materia ?></option>
-                <?php
-            endforeach;
-        ?>
-    </select>
-    <select name="aluno" id="aluno" disabled required>
-        <option value="" style="display: none;">Selecione um aluno</option>
-        <?php
-            foreach ($this->alunos as $aluno):
-                ?>
-                    <option value="<?= $aluno->cd_aluno ?>"><?= $aluno->nome_aluno ?></option>
-                <?php
-            endforeach;
-        ?>
-    </select>
-    <p>Unidade <?= UNIDADE ?></p>
-    <p id="aluno">Aluno: </p>
-    <p id="materia">Materia: </p>
-    <span>Prova</span><input type="number" name="prova" id="prova" readonly="readonly" value="0" step="0.01" min="0" max="10"> <br>
-    <span>Trabalho</span><input type="number" name="trabalho" id="trabalho" readonly="readonly" value="0" step="0.01" min="0" max="10"> <br>
-    <button type="button" id="editar">Editar</button>
-    <button type="button" id="cancelar" style="display: none;">Cancelar</button>
-    <button type="submit" id="salvar" style="display: none;">Salvar</button>
-    <p id="retorno"></p>
+        </select>
+        <select name="materia" id="materia" required disabled>
+            <option value="">Selecione uma matéria</option>
+            <?php
+                $aux = [];
+                foreach($this->professor_materias_turmas as $turma):
+                    foreach ($turma as $id_materia => $materia):
+                        if (array_key_exists($id_materia, $aux)) continue;
+                        $aux[$id_materia] = $materia;
+                        ksort($aux, SORT_NUMERIC);
+                    endforeach;
+                endforeach;
+                foreach($aux as $id_materia => $materia):
+                    ?>
+            <option value="<?= $id_materia ?>"><?= $materia ?></option>
+            <?php
+                endforeach;
+            ?>
+        </select>
+        <select name="aluno" id="aluno" disabled required>
+            <option value="" style="display: none;">Selecione um aluno</option>
+            <?php
+                foreach ($this->alunos as $aluno):
+                    ?>
+            <option value="<?= $aluno->cd_aluno ?>"><?= $aluno->nome_aluno ?></option>
+            <?php
+                endforeach;
+            ?>
+        </select>
+    </div>
+
+    <div
+        style="display:flex;flex-direction:column;background-color:var(--background-alt);border-radius:.5rem;border:1px solid var(--shadow);margin: 0 5vw 0 5vw;padding: 1vw;">
+        <p style="font-size: 3rem; font-weight: 700; margin: 0;">Unidade <?= UNIDADE ?></p>
+        <div style="border-radius: .5rem; border: 1px var(--shadow) solid;padding: 0 1vw 0 1vw;margin: 5vh 0 5vh 0;background-color:var(--background)">
+            <p id="aluno">Aluno: </p>
+            <p id="materia">Materia: </p>
+
+        </div>
+        <div style="display:flex; column-gap: 1vw;">
+            <div style="display:flex;flex-direction:column;">
+                <span>Prova</span>
+                <input type="number" name="prova" id="prova" readonly="readonly" value="0" step="0.01" min="0" max="10">
+
+            </div>
+            <div style="display:flex;flex-direction:column;">
+                <span>Trabalho</span>
+                <input type="number" name="trabalho" id="trabalho" readonly="readonly" value="0" step="0.01" min="0"
+                    max="10">
+
+            </div>
+
+            <button type="button" id="editar">Editar</button>
+            <button type="button" id="cancelar" style="display: none;">Cancelar</button>
+            <button type="submit" id="salvar" style="display: none;">Salvar</button>
+        </div>
+
+        <p id="retorno"></p>
+    </div>
     <script>
-        let professor_materia = <?= json_encode($this->professor_materias_turmas) ?>;
-        let alunos_turma = <?= json_encode($this->alunos_turma) ?>;
+    let professor_materia = <?= json_encode($this->professor_materias_turmas) ?>;
+    let alunos_turma = <?= json_encode($this->alunos_turma) ?>;
 
-        $('form').on('submit', function (e) {
-            e.preventDefault()
+    $('form').on('submit', function(e) {
+        e.preventDefault()
 
-            $.ajax({
+        $.ajax({
                 'url': '/professor/notas/aluno/salvar',
                 'type': 'POST',
                 'dataType': 'json',
                 'data': $(this).serialize()
             })
-            .done(function (data) {
+            .done(function(data) {
                 $('p#retorno').text(data.msg)
                 $('select#turma').removeAttr('readonly', 'readonly')
                 $('select#materia').removeAttr('readonly', 'readonly')
@@ -86,46 +158,46 @@
                 $('button#cancelar').css('display', 'none')
                 $('button#salvar').css('display', 'none')
             })
-            .catch(function (a) {
+            .catch(function(a) {
                 console.log(a)
             })
-        })
+    })
 
-        $('form select#turma').on('change', function () {
-            $('form select#turma option#init').remove()
-            $('form select#materia').prop('disabled', false)
-            $('form select#aluno').prop('disabled', false)
+    $('form select#turma').on('change', function() {
+        $('form select#turma option#init').remove()
+        $('form select#materia').prop('disabled', false)
+        $('form select#aluno').prop('disabled', false)
 
-            let turma = $(this).val()
-            $('form select#materia option').each(function () {
-                if ($(this).val() in professor_materia[turma]) {
-                    $(this).css('display', 'block')
-                } else {
-                    $(this).css('display', 'none')
-                }
-            })
-            $('form select#aluno option').each(function () {
-                if ($(this).val() in alunos_turma[turma]) {
-                    $(this).css('display', 'block')
-                } else {
-                    $(this).css('display', 'none')
-                }
-            })
-
-            $('form select#materia').val('')
-            $('form select#aluno').val('')
-            $('p#aluno').text('Aluno: ')
-            $('p#materia').text('Materia: ')
-            $('input#prova').val(0)
-            $('input#trabalho').val(0)
-        })
-
-        $('form select#materia').on('change', function () {
-            if ($('form select#aluno').val() == '') {
-                return
+        let turma = $(this).val()
+        $('form select#materia option').each(function() {
+            if ($(this).val() in professor_materia[turma]) {
+                $(this).css('display', 'block')
+            } else {
+                $(this).css('display', 'none')
             }
+        })
+        $('form select#aluno option').each(function() {
+            if ($(this).val() in alunos_turma[turma]) {
+                $(this).css('display', 'block')
+            } else {
+                $(this).css('display', 'none')
+            }
+        })
 
-            $.ajax({
+        $('form select#materia').val('')
+        $('form select#aluno').val('')
+        $('p#aluno').text('Aluno: ')
+        $('p#materia').text('Materia: ')
+        $('input#prova').val(0)
+        $('input#trabalho').val(0)
+    })
+
+    $('form select#materia').on('change', function() {
+        if ($('form select#aluno').val() == '') {
+            return
+        }
+
+        $.ajax({
                 'url': `/professor/notas/aluno/${$('form select#aluno').val()}/materia/${$(this).val()}`,
                 'type': 'GET',
                 'dataType': 'json'
@@ -137,14 +209,14 @@
                 $('p#aluno').text('Aluno: ' + data.notas.aluno)
                 $('p#materia').text('Materia: ' + data.notas.materia)
             })
-        })
+    })
 
-        $('form select#aluno').on('click', function () {
-            if ($('form select#materia').val() == '') {
-                return
-            }
+    $('form select#aluno').on('click', function() {
+        if ($('form select#materia').val() == '') {
+            return
+        }
 
-            $.ajax({
+        $.ajax({
                 'url': `/professor/notas/aluno/${$(this).val()}/materia/${$('form select#materia').val()}`,
                 'type': 'GET',
                 'dataType': 'json'
@@ -157,38 +229,38 @@
                 $('p#materia').text('Materia: ' + data.notas.materia)
             })
             .catch(data => console.log(data))
+    })
+
+    $('button#editar').on('click', function() {
+        if ($('select#turma').val() == '' || $('select#materia').val() == '' || $('select#aluno').val() == '') {
+            return
+        }
+        $('button#editar').css('display', 'none')
+        $('button#cancelar').css('display', 'block')
+        $('button#salvar').css('display', 'block')
+
+        $('select').each(function() {
+            $(this).attr('readonly', 'readonly')
         })
 
-        $('button#editar').on('click', function () {
-            if ($('select#turma').val() == '' || $('select#materia').val() == '' || $('select#aluno').val() == '') {
-                return
-            }
-            $('button#editar').css('display', 'none')
-            $('button#cancelar').css('display', 'block')
-            $('button#salvar').css('display', 'block')
+        var prova = $('input#prova').val()
+        var trabalho = $('input#trabalho').val()
+        $('button#cancelar').on('click', function() {
+            $('button#editar').css('display', 'block')
+            $('button#cancelar').css('display', 'none')
+            $('button#salvar').css('display', 'none')
 
-            $('select').each(function () {
-                $(this).attr('readonly', 'readonly')
+            $('select').each(function() {
+                $(this).removeAttr('readonly', 'readonly')
             })
-
-            var prova = $('input#prova').val()
-            var trabalho = $('input#trabalho').val()
-            $('button#cancelar').on('click', function () {
-                $('button#editar').css('display', 'block')
-                $('button#cancelar').css('display', 'none')
-                $('button#salvar').css('display', 'none')
-                
-                $('select').each(function () {
-                    $(this).removeAttr('readonly', 'readonly')
-                })
-                $('input#prova').attr('readonly', 'readonly')
-                $('input#prova').val(prova)
-                $('input#trabalho').val('readonly', 'readonly')
-                $('input#trabalho').val(trabalho)
-            })
-
-            $('input#prova').removeAttr('readonly', 'readonly')
-            $('input#trabalho').removeAttr('readonly', 'readonly')
+            $('input#prova').attr('readonly', 'readonly')
+            $('input#prova').val(prova)
+            $('input#trabalho').val('readonly', 'readonly')
+            $('input#trabalho').val(trabalho)
         })
+
+        $('input#prova').removeAttr('readonly', 'readonly')
+        $('input#trabalho').removeAttr('readonly', 'readonly')
+    })
     </script>
 </form>
