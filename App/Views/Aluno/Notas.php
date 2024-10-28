9b7a7df2819d34cf2b344f8fe33af236ac5cb2a6
lemelinha <lucas.leme.0806@gmail.com>
2024-10-28 20:10:43 -0300

    notas

---
+++ App/Views/Aluno/Notas.php
@@ -0,0 +1,39 @@
+<select name="materia" id="materia">
+    <option value="">Selecionar Matéria</option>
+    <?php
+        foreach ($this->materias as $materia):
+            ?>
+                <option value="<?= $materia->cd_materia ?>"><?= $materia->nm_materia ?></option>
+            <?php
+        endforeach;
+    ?>
+</select>
+<select name="unidade" id="unidade">
+    <?php
+        for ($i=1;$i<=UNIDADE;$i++):
+            ?>
+                <option value="<?= $i ?>" <?= $i==UNIDADE?'selected':'' ?>>Unidade <?= $i ?></option>
+            <?php
+        endfor;
+    ?>
+</select>
+
+<div class="notas">
+    <?php $this->renderView('tabelaNotas', 'Aluno'); ?>
+</div>
+<script>
+    $('select#materia, select#unidade').on('change', function () {
+        console.log($(this).val())
+
+        $.ajax({
+            'url': '/aluno/boletim',
+            'type': 'GET',
+            'dataType': 'json',
+            'data': {'materia': $('select#materia').val(), 'unidade': $('select#unidade').val()}
+        })
+        .done(data => {
+            $('div.notas').html(data.notas)
+        })
+        .catch(data => console.log(data))
+    })
+</script>
